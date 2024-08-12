bf7e29972963eb7cfacd55aa06fd1236319d0bbd
MahmoudZakaria <88841099+MahmoudZakarya@users.noreply.github.com>
2024-08-12 20:58:30 +0300

    right  part of the nav

---
+++ src/components/Cart.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+
+import React from 'react'
+import { Sheet } from './ui/sheet'
+
+const Cart = () => {
+  return (
+    <Sheet>
+        
+    </Sheet>
+  )
+}
+
+export default Cart
