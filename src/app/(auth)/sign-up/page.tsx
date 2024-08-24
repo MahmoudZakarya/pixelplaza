a50fb4797d56b3e03ca6720d37c93b6e2d31f8d2
MahmoudZakaria <88841099+MahmoudZakarya@users.noreply.github.com>
2024-08-24 23:58:28 +0300

    trpc installation

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -7,10 +7,24 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
+import {useForm} from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
 
 import React from 'react'
+import { AuthCredentialsValidator, TAuthCredentialsValidator } from '@/lib/validators/account-credentials'
 
 const Page = () => {
+
+   
+
+    const {register, handleSubmit, formState: {errors} } = useForm<TAuthCredentialsValidator>({
+        resolver: zodResolver(AuthCredentialsValidator)
+    })
+
+    const onSubmit = ({email, password }: TAuthCredentialsValidator) => {
+
+    }
+
   return (
     <div className='container relative flex pt-20 flex-col items-center justify-center lg:px-0'>
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] ">
@@ -21,20 +35,23 @@ const Page = () => {
             </div>
 
             <div className="grid gap-6">
-                <form >
+                <form onSubmit={handleSubmit(onSubmit)} >
                     <div className="grid gap-2">
                         <div className="grid gap-2 py-2">
                             <Label htmlFor='email'>Email</Label>
-                            <Input className={cn({
-                                "focus-visible:ring-violet-500": true
+                            <Input {...register('email')} className={cn({
+                                "focus-visible:ring-violet-500": !errors.email,
+                                "focus-visible:ring-red-500": errors.email,
+
                             })} 
                             placeholder='you@example.com' />
                         </div>
 
                         <div className="grid gap-2 py-2">
                             <Label htmlFor='password'>Password</Label>
-                            <Input className={cn({
-                                "focus-visible:ring-violet-500": true
+                            <Input {...register("password")} className={cn({
+                                "focus-visible:ring-violet-500": !errors.password,
+                                "focus-visible:ring-red-500": errors.password,
                             })} 
                             placeholder='Password' />
                         </div>
