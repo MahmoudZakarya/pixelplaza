4aa1f841126cc5dbef3c34d28f1747e52e881f28
MahmoudZakaria <88841099+MahmoudZakarya@users.noreply.github.com>
2024-08-24 22:53:59 +0300

    signup3

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,4 +1,13 @@
+'use client'
+
+
 import { Icons } from '@/components/Icons'
+import { Button, buttonVariants } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+
 import React from 'react'
 
 const Page = () => {
@@ -7,12 +16,36 @@ const Page = () => {
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] ">
             <div className="flex flex-col text-center items-center space-y-2">
                 <Icons.logo className='h-20 w-20' />
-                
+                <h1 className='text-2xl font-bold'>Create an account</h1>
+                <Link className={buttonVariants({variant:'link',})} href='/sign-in'>Already have an account? Sign In</Link>
+            </div>
+
+            <div className="grid gap-6">
+                <form >
+                    <div className="grid gap-2">
+                        <div className="grid gap-2 py-2">
+                            <Label htmlFor='email'>Email</Label>
+                            <Input className={cn({
+                                "focus-visible:ring-violet-500": true
+                            })} 
+                            placeholder='you@example.com' />
+                        </div>
+
+                        <div className="grid gap-2 py-2">
+                            <Label htmlFor='password'>Password</Label>
+                            <Input className={cn({
+                                "focus-visible:ring-violet-500": true
+                            })} 
+                            placeholder='Password' />
+                        </div>
+                        <Button>Sign Up</Button>
+                    </div>
+                </form>
             </div>
         </div>
       
     </div>
-  )
+  )  
 }
 
 export default Page
