4ac5b88356e997307ea2b4c4f6632a7a5a8acc72
MahmoudZakaria <88841099+MahmoudZakarya@users.noreply.github.com>
2024-08-30 20:36:23 +0300

    creating database tables and access logic

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,4 +1,4 @@
-import { PRODUCT_CATEGORIES } from "@/Config";
+import { PRODUCT_CATEGORIES } from "../../Config/index";
 import { CollectionConfig } from "payload/types";
 
 export const Products: CollectionConfig = {
@@ -51,6 +51,77 @@ export const Products: CollectionConfig = {
       required: true,
       relationTo: "product_files",
       hasMany: false,
+    },
+    {
+      name: "approvedForSale",
+      label: "Product Status",
+      type: "select",
+      defaultValue: "pending",
+      access: {
+        create: ({ req }) => req.user.role === "admin",
+        read: ({ req }) => req.user.role === "admin",
+        update: ({ req }) => req.user.role === "admin",
+      },
+      options: [
+        {
+          label: "Pending verification",
+          value: "pending",
+        },
+        {
+          label: "Approved",
+          value: "approved",
+        },
+        {
+          label: "Denied",
+          value: "denied",
+        },
+      ],
+    },
+    {
+      name: "priceId",
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: "text",
+      admin: {
+        hidden: true,
+      },
+    },
+
+    {
+      name: "stripeId",
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: "text",
+      admin: {
+        hidden: true,
+      },
+    },
+    {
+      name: "images",
+      type: "array",
+      label: "Product images",
+      minRows: 1,
+      maxRows: 4,
+      required: true,
+      labels: {
+        singular: "Image",
+        plural: "Images",
+      },
+
+      fields: [
+        {
+          name: "image",
+          type: "upload",
+          relationTo: "media",
+          required: true,
+        },
+      ],
     },
   ],
 };
